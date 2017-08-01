76d6c8897f90ca77903cfd255b9d0f79e1e591e2
Your Name <you@example.com>
2017-08-01 08:20:53 +0800

    实验完成

--- src/assert.js
+++ src/assert.js
@@ -11,10 +11,10 @@ var b = 3;
  // assert.deepEqual(a,b);
 
 
-//3.deepStrictEqual.他和上面的一样,但是他判断用的是===
+//3.assert.deepStrictEqual.他和上面的一样,但是他判断用的是===
 // assert.deepStrictEqual(a,b);
 
-//4.doesNotThrow (参数1:(function),第三个参数和第二个参数比较类型)
+//4.assert.doesNotThrow (参数1:(function),第三个参数和第二个参数比较类型)
 //如果错误类型不相同,打印错误,相同打印类型加拼接参数
 // assert.doesNotThrow(function () {
 //
@@ -56,7 +56,108 @@ var b = 3;
 
 
 
-//8.assert.notDeepEqual
+//8.assert.notDeepEqual测试是否不深度相等。 与 assert.deepEqual() 相反。
+//如果两个值深度相等，则抛出一个带有 message 属性的 AssertionError，其中 message 属性的值等于传入的 message 参数的值。 如果 message 参数为 undefined，则赋予默认的错误信息。
+const obj1 = {
+    a: {
+        b: 1
+    }
+};
+const obj2 = {
+    a: {
+        b: 2
+    }
+};
+const obj3 = {
+    a: {
+        b: 1
+    }
+};
+const obj4 = Object.create(obj1);
+
+// 9.assert.notDeepEqual(obj1, obj1);
+//测试是否不深度全等。
+// 抛出 AssertionError: { a: { b: 1 } } notDeepEqual { a: { b: 1 } }
+//如果两个值深度全等，则抛出一个带有 message 属性的 AssertionError，其中 message 属性的值等于传入的 message 参数的值。 如果 message 参数为 undefined，则赋予默认的错误信息
+
+//10.assert.notEqual(actual, expected[, message])
+//使用 不等运算符（!=）测试是否不相等。
+//如果两个值相等，则抛出一个带有 message 属性的 AssertionError，其中 message 属性的值等于传入的 message 参数的值。 如果 message 参数为 undefined，则赋予默认的错误信息。
+
+// assert.notEqual(1, 2);
+// 通过
+
+// assert.notEqual(1, 1);
+// 抛出 AssertionError: 1 != 1
+
+// assert.notEqual(1, '1');
+// 抛出 AssertionError: 1 != '1'
+
+//11.assert.ok(value[, message])
+//测试 value 是否为真值。 相当于 assert.equal(!!value, true, message)。
+//如果 value 不为真值，则抛出一个带有 message 属性的 AssertionError，其中 message 属性的值等于传入的 message 参数的值。 如果 message 参数为 undefined，则赋予默认的错误信息。
+
+// assert.ok(true);
+// 通过
+// assert.ok(1);
+// 通过
+// assert.ok(false);
+// 抛出 "AssertionError: false == true"
+// assert.ok(0);
+// 抛出 "AssertionError: 0 == true"
+// assert.ok(false, '不是真值');
+// 抛出 "AssertionError: 不是真值"
+
+
+//12.assert.strictEqual(actual, expected[, message])
+//使用 全等运算符（===）测试是否全等。
+
+//如果两个值不全等，则抛出一个带有 message 属性的 AssertionError，其中 message 属性的值等于传入的 message 参数的值。 如果 message 参数为 undefined，则赋予默认的错误信息。
+
+// assert.strictEqual(1, 2);
+// 抛出 AssertionError: 1 === 2
+
+// assert.strictEqual(1, 1);
+// 通过
+
+// assert.strictEqual(1, '1');
+// 抛出 AssertionError: 1 === '1'
+
+
+//13.assert.throws(block[, error][, message])
+/*
+* 期望 block 函数抛出错误。
+
+ 如果指定了 error，error 可以是构造函数、正则表达式、或自定义的验证函数。
+
+ 如果指定了 message，则当 block 不抛出错误时，message 会作为 AssertionError 的错误信息。
+ **/
+
+
+//注意，error 不能是一个字符串。 如果第二个参数是一个字符串，则视为省略 error 参数，传入的字符串会被用于 message。 这点比较容易搞错：
+
+// 这是错误的！不要这么做！
+//assert.throws(myFunction, '错误', '没有抛出期望的信息');
+
+// 应该这么做。
+//assert.throws(myFunction, /错误/, '没有抛出期望的信息');
+
+
+//14.assert.notStrictEqual(actual, expected[, message])
+//使用 不全等运算符（!==）测试是否不全等。与 assert.deepStrictEqual() 相反。
+
+
+// assert.notStrictEqual(1, 2);
+// 通过
+
+// assert.notStrictEqual(1, 1);
+// 抛出 AssertionError: 1 !== 1
+
+// assert.notStrictEqual(1, '1');
+// 通过
+//如果两个值全等，则抛出一个带有 message 属性的 AssertionError，其中 message 属性的值等于传入的 message 参数的值。 如果 message 参数为 undefined，则赋予默认的错误信息。
+
+
 
 
 
